c08b2584c799db3ed1dba52317ea7e74baff1510
maevep <potter.maeve@gmail.com>
2016-08-18 12:13:57 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,9 +4,9 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
-var castles = ["museums", "bridges", "cities"]
+var things = ["Birch", "Maple", "Cedar", "Pine", "Redwood", "Sequoia"];
+var programmers = ["baker", "artist", "police man", "Fire fighter", "Builder", "Farmer"]
+var castles = ["Home", "Shack", "Boat"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
 
